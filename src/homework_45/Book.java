db9d6d3792760f8fec3948ba4dbe79fcebd4aecf
Leon Zuev <loonatune@gmail.com>
2023-03-22 17:25:42 +0200

    Add constructor and getters

--- src/homework_45/Book.java
+++ src/homework_45/Book.java
@@ -1,7 +1,7 @@
 package homework_45;
 /*
-Создайте класс Book (книга), в конструктор которого передайте:
-автора, название книги и количество страниц.
++Создайте класс Book (книга), в конструктор которого передайте:
++автора, название книги и количество страниц.
 Реализуйте интерфейс для сравнения книг:
 они должны сортироваться по авторам,
 а если авторы совпадают - по названиям
@@ -9,4 +9,25 @@ package homework_45;
  */
 
 public class Book {
+  private String author;
+  private String title;
+  private int numPages;
+
+  public Book(String author, String title, int numPages) {
+    this.author = author;
+    this.title = title;
+    this.numPages = numPages;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getNumPages() {
+    return numPages;
+  }
 }
