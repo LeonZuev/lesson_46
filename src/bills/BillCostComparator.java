ed57c638010e3e6efe31e9d03c38f1883e6c7dd2
Leon Zuev <loonatune@gmail.com>
2023-03-22 21:59:13 +0200

    Add BillCostComparator

---
+++ src/bills/BillCostComparator.java
@@ -0,0 +1,15 @@
+package bills;
+
+import java.util.Comparator;
+
+public class BillCostComparator implements Comparator<Bill> {
+  @Override
+  public int compare(Bill o1, Bill o2) {
+    Double cost1 = o1.getCost();
+    Double cost2 = o2.getCost();
+    if (!cost1.equals(cost2)) {
+      return cost1.compareTo(cost2);
+    }
+    return o1.getService().compareTo(o2.getService());
+  }
+}
