24368328c132a697d7724fa814e7265590edfcd2
Leon Zuev <loonatune@gmail.com>
2023-03-23 23:49:21 +0200

    Add TitleAuthorComparatorTest with description

---
+++ src/homework_45/TitleAuthorComparatorTest.java
@@ -0,0 +1,12 @@
+package homework_45;
+/*
+Напишите автотесты с использованием JUnit для задачи 1 из предыдущего домашнего задания.
+Проверьте краевые случаи, предусмотрите разные варианты.
+    a:null               b:"space "
+title 0:0 author 0:0  title 0:0 author 0:0
+      1:0        1:0        1:0        1:0
+      0:1        0:1        0:1        0:1
+ */
+
+public class TitleAuthorComparatorTest {
+}
