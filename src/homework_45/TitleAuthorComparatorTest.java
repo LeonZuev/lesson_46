9ae96b5f2d40525f2add2bb8097e95dc00a0e41c
Leon Zuev <loonatune@gmail.com>
2023-03-24 17:04:12 +0300

    Add books

--- src/homework_45/TitleAuthorComparatorTest.java
+++ src/homework_45/TitleAuthorComparatorTest.java
@@ -24,16 +24,31 @@ public class TitleAuthorComparatorTest {
     Book book1 = new Book("", "",1);
     Book book2 = new Book("", "A",1);
     Book book3 = new Book("A", "",1);
+    Book book4 = new Book(" "," ",1);
+    Book book5 = new Book(" ","A",1);
+    Book book6 = new Book("A"," ",1);
+    Book book7 = new Book("A","B",1);
+    Book book8 = new Book("B","A",1);
 
     List<Book> actual = new ArrayList<>();
     actual.add(book1);
     actual.add(book2);
     actual.add(book3);
+    actual.add(book4);
+    actual.add(book5);
+    actual.add(book6);
+    actual.add(book7);
+    actual.add(book8);
 
     List<Book> expected = new ArrayList<>();
     expected.add(book1);
     expected.add(book2);
     expected.add(book3);
+    expected.add(book4);
+    expected.add(book5);
+    expected.add(book6);
+    expected.add(book7);
+    expected.add(book8);
 
     actual.sort(new TitleAuthorComparator());
 
